257580051c0262dbb4030bd78b4682d3a98f5fed
itorlou <isaactorlou@gmail.com>
2023-08-28 18:53:01 +0200

    reorder

---
+++ 1.- Introducción/3.- Aritmética/Numeros.py
@@ -0,0 +1,11 @@
+# enteros y flotantes
+num1 = 40
+num2 = 99.99
+
+print(type(num1))
+print(type(float(num1))) 
+print(float(num1)) #casteamos a float
+
+print(type(num2))
+print(type(int(num2))) 
+print(int(num2)) #casteamos a integer

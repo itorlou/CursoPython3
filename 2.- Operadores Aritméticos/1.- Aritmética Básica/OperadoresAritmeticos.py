6ea89fc696910f47a2db6687d64bc9e819adc56f
itorlou <isaactorlou@gmail.com>
2023-08-29 17:06:57 +0200

    Sección 4 del curso, Aritmética

---
+++ 2.- Operadores Aritméticos/1.- Aritmética Básica/OperadoresAritmeticos.py
@@ -0,0 +1,17 @@
+# operadores básicos
+
+print(1+1) #suma 
+
+print(150-75) #resta 
+
+print(10*10) #multiplicación 
+
+print(75/9) #división "normal"
+
+# operadores avanzados
+
+print (2 ** 3) #exponente 2*2*2
+
+print(75//9) #división entera (solo devuelve la parte entera del cociente sin decimales)
+
+print (100 % 100) # módulo (toma el residuo)
---
+++ 2.- Operadores Aritméticos/2.- Aritmética con Variables/AritmeticaVariables.py
@@ -0,0 +1,25 @@
+# variable va a ser un dato al que se le asigna un nombre
+# operador aritmético 
+num1 = 10
+num2 = 5
+# mostramos valor inicial de la variable
+print (num1)
+print (num2)
+'''
+print (num1 + num2) # suma con variables
+print (num1 - num2) # resta con variables
+print (num1 * num2) # multiplicación con variables
+print (num1 / num2) # división con variables
+print (num1 // num2) # división entera con variables
+print (num1 ** num2) # exponenciación con variables
+print (num1 % num2) # módulo con variables
+'''
+# podemos crear variables que contengan operaciones
+
+suma = num1 + num2
+
+print (suma)
+
+resta = num1 - num2 
+
+print (resta)

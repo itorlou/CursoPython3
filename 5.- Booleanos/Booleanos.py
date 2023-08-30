ad05f38660eeced69ff4be067f29bf194fe97e8d
itorlou <isaactorlou@gmail.com>
2023-08-30 17:54:25 +0200

    Booleanos

---
+++ 5.- Booleanos/Booleanos.py
@@ -0,0 +1,5 @@
+# tipo de dato que puede tener solo dos valores (true o false)
+verdadero = True #esto ya es un dato booleano al tener la variable reservada
+falso = False
+
+print (type(verdadero))
---
+++ 5.- Booleanos/OperadoresRelacionales.py
@@ -0,0 +1,21 @@
+# son comparaciones
+# == != < > <= >=
+
+num1 = 1000
+num2 = 500
+
+print(num1 > num2) #devuelve un boolean 
+print(num2 < num1)
+
+num3=1000
+
+print (num1 >= num3)
+print (num1 <= num3)
+print (num1 == num3)
+
+print (num1 != num2)
+
+cadena= "Comparaciones de Strings"
+cadena2="Comparacion de Strings"
+
+print (cadena > cadena2)
